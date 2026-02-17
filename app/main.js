4518ced3fd819803c08005328506278e4f67f5ae
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-02-17 19:05:09 +0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,13 +1,25 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+// Debug print
 console.log("Logs from your program will appear here!");
 
-// TODO: Uncomment the code below to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
+// Minimal TCP server
+const server = net.createServer((socket) => {
+  console.log("Client connected");
+
+  // Handle client closing connection
+  socket.on("close", () => {
+    console.log("Client disconnected");
+    socket.end();
+  });
+
+  // Optional: log any data received
+  socket.on("data", (data) => {
+    console.log("Received data:\n", data.toString());
+  });
+});
+
+// Listen on port 4221
+server.listen(4221, "localhost", () => {
+  console.log("Server listening on localhost:4221");
+});
